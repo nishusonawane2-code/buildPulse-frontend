320d456df9afa4954c74a0e1f97b7e40bcf66442
nishusonawane2-code <nishusonawane2@gmail.com>
2026-02-13 18:03:21 +0530

    chnage name

--- src/views/FloorPlans.jsx
+++ src/views/FloorPlans.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import ConsultPopup from "./consultpopup";
+import ConsultPopup from "./ConsultPopUp";
 
 const floorPlans = [
   {
